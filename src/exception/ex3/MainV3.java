2c3fb6a8f319b5a82dfd9ed792a79341707cdace
Aunar614 <developkim614@gmail.com>
2025-02-17 14:00:07 +0530

    예외 계층2 - 활용

--- src/exception/ex3/MainV3.java
+++ src/exception/ex3/MainV3.java
@@ -8,11 +8,8 @@ public class MainV3 {
 
     public static void main(String[] args) throws NetworkClientExceptionV3 {
         Scanner sc = new Scanner(System.in);
-        // NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
-        // NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
-        // NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
-        // NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
-        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        // NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        NetworkServiceV3_2 networkService = new NetworkServiceV3_2();
 
         while (true) {
             System.out.print("전송할 문자 : ");
